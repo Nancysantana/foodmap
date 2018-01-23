c60c15625d6cc2d7b73bc35ca13caf21599ffa9f
Nancysantana <yaretsita.27@gmail.com>
2018-01-23 11:14:40 -0600

    jquery

--- js/filter.js
+++ js/filter.js
@@ -1,6 +1,26 @@
-// var $input = $("#filter_restorant");
+var $input = $("#filter_restorant");
+var $name = $(".modal-title");
+var $food = $(".food");
+var $adress = $("adress");
+var $service = $(".service");
+var $photo = $(".imags");
+var $service = $("button");
 
 
+function loadPage() {
+  $("#myModal").modal();
+  $photo.ckick(selectPhoto);
+  $input.keyup(nameRestaurant);
+}
 
+function selectPhoto() {
+  var $restaurant = event.target.dataset.name;
+  var $dataRestaurant = data[$restaurant];
+
+  var $nameTitle = $dataRestaurant["name"];
+  var $adreesUb = $dataRestaurant["adress"];
+  var $foodEspecial = $dataRestaurant["food"];
+  var $
+}
 
 $(document).ready(loadPage);
