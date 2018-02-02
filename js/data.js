bf2f15423632a575d0d00320286f83e469684f14
Nancysantana <yaretsita.27@gmail.com>
2018-02-01 22:16:12 -0600

    js

--- js/data.js
+++ js/data.js
@@ -1,75 +1,75 @@
 //Data
-var data = {
-  'El Rincon Mexicano' : {
-    'name': 'El Rincon Mexicano ',
-    'adress':'Plaza Baratillo, 4a Calle de La Alameda, Centro, 36000',
-    'food': 'Mexicana',
-    'photo': '../assets/images/enchiladas.png',
-    'specialty food' : 'Enchiladas',
-    'services': 'no contamos con servicio a domicilio'
-  },
-  'La Casa de Toño' : {
-    'name': 'La Casa de Toño',
-    'adress':'Positos 35, Zona Centro, 36000 Guanajuato, Gto.',
-    'food': 'Mexicana',
-    'photo': '../assets/images/pozole.png',
-    'services': 'contamos con servicio a domicilio'
-  },
-   'La perla' : {
-    'name': 'La perla',
-    'adress':'Calle de Arriba #6, San Javier, 36020 CDMX',
-    'food': 'Italiana',
-    'photo': '../assets/images/subway.png',
-    'specialty food' : 'Subway',
-    'services': 'no contamos con servicio a domicilio'
- },
- 'Italiano' : {
-    'name': 'name': 'Italiano'
-    'adress':'Jardín de la Unión N. 01. Terraza del Hotel San Diego, Centro Histórico.  Terraza Zona Sur Blvd.Euquerio Guerrero 6471 C.P. 36000.',
-    'food': 'Italiana',
-    'photo': '../assets/images/sandwich.png',
-    'specialty food' : 'Sándwiches, Cocina toscana, Licores',
-    'services': 'contamos con servicio a domicilio'
-  },
-  'Yelp' : {
-    'name': 'Yelp',
-    'adress':'Milán 44 06006 México, D.F. juárez',
-    'food': 'Italiana',
-    'photo': '../assets/images/pizza.png',
-    'specialty food' : 'Pizzería, Fábrica de cerveza, Postres',
-    'services': 'contamos con servicio a domicilio'
+var restaurants = [
+    {
+      'name': 'El Rincon Mexicano ',
+      'adress':'Plaza Baratillo, 4a Calle de La Alameda, Centro, 36000',
+      'food': 'Mexicana',
+      'photo': '../assets/images/enchiladas.png',
+      'specialty food' : 'Enchiladas',
+      'services': 'no contamos con servicio a domicilio'
+    },
+    {
+      'name': 'La Casa de Toño',
+      'adress':'Positos 35, Zona Centro, 36000 Guanajuato, Gto.',
+      'food': 'Mexicana',
+      'photo': '../assets/images/pozole.png',
+      'services': 'contamos con servicio a domicilio'
+    },
+    {
+      'name': 'La perla',
+      'adress':'Calle de Arriba #6, San Javier, 36020 CDMX',
+      'food': 'Italiana',
+      'photo': '../assets/images/subway.png',
+      'specialty food' : 'Subway',
+      'services': 'no contamos con servicio a domicilio'
+    },
+    {
+      'name': 'Italiano',
+      'adress':'Jardín de la Unión N. 01. Terraza del Hotel San Diego, Centro Histórico.  Terraza Zona Sur Blvd.Euquerio Guerrero 6471 C.P. 36000.',
+      'food': 'Italiana',
+      'photo': '../assets/images/sandwich.png',
+      'specialty food' : 'Sándwiches, Cocina toscana, Licores',
+      'services': 'contamos con servicio a domicilio'
+    },
+    {
+      'name': 'Yelp',
+      'adress':'Milán 44 06006 México, D.F. juárez',
+      'food': 'Italiana',
+      'photo': '../assets/images/pizza.png',
+      'specialty food' : 'Pizzería, Fábrica de cerveza, Postres',
+      'services': 'contamos con servicio a domicilio'
 
-  },
-  'yuniku' : {
-    'name': 'yuniku',
-    'adress':'5 de Mayo 52 Col. Centro 06000 México, D.F. Centro Sur',
-    'food': 'Japonesa',
-    'photo': '../assets/images/suchi.png',
-    'specialty food' : 'Sushi',
-    'services': 'no contamos con servicio a domicilio'
- },
- 'ozu' : {
-    'name': 'ozu',
-    'adress':'Paseo5 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
-    'food': 'Japonesa',
-    'photo': '../assets/images/curry.png',
-    'specialty food' : 'Curry japonés, Sopas',
-    'services': 'contamos con servicio a domicilio'
-  },
-  'Oishi' : {
-    'name': 'Oishi',
-    'adress':'Del Truco 7, Zona Centro, 36000 ',
-    'food': 'Japonesa',
-    'photo': '../assets/images/ramen.png',
-    'specialty food' : 'Ramen, Noodles',
-    'services': 'no contamos con servicio a domicilio'
-  },
-  ' Tsujihan' : {
-    'name': ' Tsujihan',
-    'adress':'Del Truco 8, Zona Centro, 36600 ',
-    'food': 'Japonesa',
-    'photo': '../assets/images/Tonkatsu.png',
-    'specialty food' : ' Tonkatsu',
-    'services': 'contamos con servicio a domicilio'
-  }
-}
+    },
+    {
+      'name': 'yuniku',
+      'adress':'5 de Mayo 52 Col. Centro 06000 México, D.F. Centro Sur',
+      'food': 'Japonesa',
+      'photo': '../assets/images/suchi.png',
+      'specialty food' : 'Sushi',
+      'services': 'no contamos con servicio a domicilio'
+    },
+    {
+      'name': 'ozu',
+      'adress':'Paseo5 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
+      'food': 'Japonesa',
+      'photo': '../assets/images/curry.png',
+      'specialty food' : 'Curry japonés, Sopas',
+      'services': 'contamos con servicio a domicilio'
+    },
+    {
+      'name': 'Oishi',
+      'adress':'Del Truco 7, Zona Centro, 36000 ',
+      'food': 'Japonesa',
+      'photo': '../assets/images/ramen.png',
+      'specialty food' : 'Ramen, Noodles',
+      'services': 'no contamos con servicio a domicilio'
+    },
+    {
+      'name': ' Tsujihan',
+      'adress':'Del Truco 8, Zona Centro, 36600 ',
+      'food': 'Japonesa',
+      'photo': '../assets/images/Tonkatsu.png',
+      'specialty food' : ' Tonkatsu',
+      'services': 'contamos con servicio a domicilio'
+    }
+  ];
--- js/filter.js
+++ js/filter.js
@@ -1,11 +1,36 @@
-var $input = $("#filter_restorant");
-var $name = $(".modal-title");
-var $food = $(".food");
-var $adress = $("adress");
-var $service = $(".service");
-var $photo = $(".images");
-var $service = $(".button");
+function loadPage() {
+	paintRestaurants(restaurants);
+	// $('#search').keyup(filterRestaurants);
+	$('.modal').modal();
+}
 
+
+  function paintRestaurants(array) {
+    var template =   '<div class="food">'+
+        '<img src="__src__"  class="modal fade" data-src = "__src__" data-name = "__name__" data-adress = "__adress__" data-food = "__food__" data-specialty-food = "__specialty-food__" data-services = "__services__" alt="Photo food">'+
+      '</div>'
+    var finalTemplate = "";
+    array.forEach(function(restaurants){
+      var $images = $('<img />');
+      $images.attr('src',restaurants['photo']);
+      finalTemplate += template.replace("__src__",restaurants.photo)
+                               .replace("__name__",restaurants.name)
+                               .replace("__adress__",restaurants.adress)
+                               .replace("__food__",restaurants.food)
+                               .replace("__specialty-food__",restaurants['specialty-food'])
+                               .replace("__services__",restaurants.services);
+    });
+
+    $("#contenedor").html(finalTemplate);
+  }
+/*
+  var $input = $("#filter_restorant");
+  var $name = $(".modal-title");
+  var $food = $(".food");
+  var $adress = $("adress");
+  var $service = $(".service");
+  var $photo = $(".images");
+  var $service = $(".button");
 
 function loadPage() {
   $("#myModal").modal();
@@ -22,6 +47,7 @@ function selectPhoto() {
   var $adreesUb = $dataRestaurant["adress"];
   var $foodEspecial = $dataRestaurant["food"];
 }
+*/
 
 
 $(document).ready(loadPage);
