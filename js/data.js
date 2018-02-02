74426880dac8b8a269658be801b19d7e9c3edcc4
Nancysantana <yaretsita.27@gmail.com>
2018-02-02 04:53:00 -0600

    footmap terminado

--- js/data.js
+++ js/data.js
@@ -5,7 +5,7 @@ var restaurants = [
       'adress':'Plaza Baratillo, 4a Calle de La Alameda, Centro, 36000',
       'food': 'Mexicana',
       'photo': '../assets/images/enchiladas.png',
-      'specialty food' : 'Enchiladas',
+      'specialty-food' : 'Enchiladas',
       'services': 'no contamos con servicio a domicilio'
     },
     {
@@ -13,6 +13,7 @@ var restaurants = [
       'adress':'Positos 35, Zona Centro, 36000 Guanajuato, Gto.',
       'food': 'Mexicana',
       'photo': '../assets/images/pozole.png',
+      'specialty-food' : 'Pozole de cabeza grande',
       'services': 'contamos con servicio a domicilio'
     },
     {
@@ -20,7 +21,7 @@ var restaurants = [
       'adress':'Calle de Arriba #6, San Javier, 36020 CDMX',
       'food': 'Italiana',
       'photo': '../assets/images/subway.png',
-      'specialty food' : 'Subway',
+      'specialty-food' : 'Subway',
       'services': 'no contamos con servicio a domicilio'
     },
     {
@@ -28,7 +29,7 @@ var restaurants = [
       'adress':'Jardín de la Unión N. 01. Terraza del Hotel San Diego, Centro Histórico.  Terraza Zona Sur Blvd.Euquerio Guerrero 6471 C.P. 36000.',
       'food': 'Italiana',
       'photo': '../assets/images/sandwich.png',
-      'specialty food' : 'Sándwiches, Cocina toscana, Licores',
+      'specialty-food' : 'Sándwiches, Cocina toscana, Licores',
       'services': 'contamos con servicio a domicilio'
     },
     {
@@ -36,7 +37,7 @@ var restaurants = [
       'adress':'Milán 44 06006 México, D.F. juárez',
       'food': 'Italiana',
       'photo': '../assets/images/pizza.png',
-      'specialty food' : 'Pizzería, Fábrica de cerveza, Postres',
+      'specialty-food' : 'Pizzería, Fábrica de cerveza, Postres',
       'services': 'contamos con servicio a domicilio'
 
     },
@@ -44,8 +45,8 @@ var restaurants = [
       'name': 'yuniku',
       'adress':'5 de Mayo 52 Col. Centro 06000 México, D.F. Centro Sur',
       'food': 'Japonesa',
-      'photo': '../assets/images/suchi.png',
-      'specialty food' : 'Sushi',
+      'photo': '../assets/images/sushi.png',
+      'specialty-food' : 'Sushi',
       'services': 'no contamos con servicio a domicilio'
     },
     {
@@ -53,7 +54,7 @@ var restaurants = [
       'adress':'Paseo5 de la Reforma 250, Juárez. Dentro del Edificio Capital Reforma, esq. con Niza.',
       'food': 'Japonesa',
       'photo': '../assets/images/curry.png',
-      'specialty food' : 'Curry japonés, Sopas',
+      'specialty-food' : 'Curry japonés, Sopas',
       'services': 'contamos con servicio a domicilio'
     },
     {
@@ -61,7 +62,7 @@ var restaurants = [
       'adress':'Del Truco 7, Zona Centro, 36000 ',
       'food': 'Japonesa',
       'photo': '../assets/images/ramen.png',
-      'specialty food' : 'Ramen, Noodles',
+      'specialty-food' : 'Ramen, Noodles',
       'services': 'no contamos con servicio a domicilio'
     },
     {
@@ -69,7 +70,7 @@ var restaurants = [
       'adress':'Del Truco 8, Zona Centro, 36600 ',
       'food': 'Japonesa',
       'photo': '../assets/images/Tonkatsu.png',
-      'specialty food' : ' Tonkatsu',
+      'specialty-food' : ' Tonkatsu',
       'services': 'contamos con servicio a domicilio'
     }
   ];
--- js/filter.js
+++ js/filter.js
@@ -1,28 +1,68 @@
+//Cuando se cargue la pagina  $(document).ready(loadPage) llamara a la funcion loadPage la cual contiene el resto de las funciones
 function loadPage() {
+  // Esta funcion pinta dinamicamente las imagenes en el contenedor contiene como parametro a la data
 	paintRestaurants(restaurants);
-	// $('#search').keyup(filterRestaurants);
-	$('.modal').modal();
+  // Esta funcion filtra en este caso por nombre
+  $('#filterRestaurants').keyup(filterRestaurants);
+  // Esta funcion llama al modal dde bootstrap
+	$('#myModal').modal(e);
+  // Temporalmente previniendo error de carga con el modal
+	e.preventDefault();
 }
 
-
+  // Resive un como parametro un array por medio de la data
   function paintRestaurants(array) {
+    // Guardamos en una variable la plantilla que se mostrara en el contenedor del html
     var template =   '<div class="food">'+
-        '<img src="__src__"  class="modal fade" data-src = "__src__" data-name = "__name__" data-adress = "__adress__" data-food = "__food__" data-specialty-food = "__specialty-food__" data-services = "__services__" alt="Photo food">'+
-      '</div>'
+        								'<img src="__src__"  data-toggle="modal" data-target="#myModal" class="imagesFood" data-src="__src__" data-name="__name__" data-adress="__adress__" data-food="__food__" data-specialty-food="__specialty-food__" data-services="__services__" alt="Photo food">'+
+      								'</div>'
     var finalTemplate = "";
+    // El forEach va iterar en el array de objetos
     array.forEach(function(restaurants){
+      // aqui llamamos a la imagen creada arriba y le agregamos como atributo un src que incluira la foto que contiene la data
       var $images = $('<img />');
-      $images.attr('src',restaurants['photo']);
-      finalTemplate += template.replace("__src__",restaurants.photo)
-                               .replace("__name__",restaurants.name)
-                               .replace("__adress__",restaurants.adress)
-                               .replace("__food__",restaurants.food)
-                               .replace("__specialty-food__",restaurants['specialty-food'])
-                               .replace("__services__",restaurants.services);
+      $images.attr('src', restaurants['photo']);
+      // remplazando los valores
+      finalTemplate += template.replace("__src__", restaurants.photo)
+                               .replace("__name__", restaurants.name)
+                               .replace("__adress__", restaurants.adress)
+                               .replace("__food__", restaurants.food)
+                               .replace("__specialty-food__", restaurants['specialty-food'])
+                               .replace("__services__", restaurants.services);
     });
-
+    // pintando en el html
     $("#contenedor").html(finalTemplate);
   }
+
+  // Extraemos la data llamandola por jquery
+	function selectPhoto() {
+		var $name = $(this).data('name');
+		var $adress = $(this).data('adress');
+		var $food = $(this).data('food');
+		var $specialtyFood = $(this).data('specialty-food');
+		var $services = $(this).data('services');
+    // pintamos la informacion por medio de text y agregamosa a los contenedores del modal la informacion de las variables
+		$('.modal-title').text($name);
+		$('.adress').text($adress);
+		$('.specialtyFood').text($specialtyFood);
+		$('.service').text($services);
+	}
+
+ // Por medio de esta funcion filtraremos los restaurantes por medio de su nombre
+	function filterRestaurants() {
+		if ($('#filterRestaurants').val().trim().length > 0) { //trim nos dice la longitud del valor sin contar espacios vacios de nuestro input
+			var inputRestaurants = $('#filterRestaurants').val().toLowerCase();//Convertimos a minusculas el caracter ingresado
+			var filter = restaurants.filter(function(restaurants){//para filtrar elementos de un array utilizamos .filter
+				return restaurants.name.toLowerCase().indexOf(inputRestaurants) >= 0;
+			});
+			paintRestaurants(filter);
+      // No mostramos nada si no hay ninguna coinsidencia
+		} else {
+			$('#contenedor').empty();
+			paintRestaurants(restaurants);
+		}
+	};
+
 /*
   var $input = $("#filter_restorant");
   var $name = $(".modal-title");
@@ -48,6 +88,7 @@ function selectPhoto() {
   var $foodEspecial = $dataRestaurant["food"];
 }
 */
-
-
+// Aqui se crea un evento a elementos creados dinamicamente
+// Se utiliza .on prepara al documento para recivir informacion al llevar acabo el evento
+$(document).on('click', '.imagesFood', selectPhoto);
 $(document).ready(loadPage);
